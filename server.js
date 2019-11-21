2280e26178da2b3d3c4f5991d893d42341981a7f
Admin <masashilandjob@gmail.com>
2019-11-21 19:16:33 +1300

    initial commit

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ const path = require("path");
 const userRoutes = require("./Routes/user");
 const fileUpload = require("express-fileupload");
 const multer = require("multer");
-// app.use(express.static(path.join(__dirname, "build")));
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 // heroku ,port 3001-> 3000, client json package proxy also 3001->3000
 //mongodb+srv://admin:admin@cluster0-xngyq.mongodb.net/test?retryWrites=true&w=majority
 //mongodb://localhost:27017/server
@@ -41,59 +47,60 @@ app.use(cookieParser());
 
 app.use("/api", userRoutes);
 
-const storage = multer.diskStorage({
-  destination: "./public/uploads/",
-  filename: function(req, file, cd) {
-    cd(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    ); //jpeg , gif
-    //1 null, file name,timestamp,inputname//file.fieldname
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: "./public/uploads/",
+//   filename: function(req, file, cd) {
+//     cd(
+//       null,
+//       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+//     ); //jpeg , gif
+//     //1 null, file name,timestamp,inputname//file.fieldname
+//   }
+// });
 //init upload
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 },
-  fileFilter: function(req, file, cb) {
-    checkFileType(file, cb);
-  }
-}).single("myImage"); //field name //multiple use array html enctype= 'multipart/form-data
-function checkFileType(file, cb) {
-  // Allowed extensions
-  const filetypes = /jpeg|jpg|png|gif/;
-  //check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  //check mime
-  const mimetype = filetypes.test(file.mimetype);
 
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cd("Error: Images only!");
-  }
-}
+// const upload = multer({
+//   storage: storage,
+//   limits: { fileSize: 1000000 },
+//   fileFilter: function(req, file, cb) {
+//     checkFileType(file, cb);
+//   }
+// }).single("myImage"); //field name //multiple use array html enctype= 'multipart/form-data
+// function checkFileType(file, cb) {
+//   // Allowed extensions
+//   const filetypes = /jpeg|jpg|png|gif/;
+//   //check ext
+//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+//   //check mime
+//   const mimetype = filetypes.test(file.mimetype);
 
-app.post("/upload", (res, req) => {
-  upload(req, res, err => {
-    if (err) {
-      res.render("index", {
-        msg: err
-      });
-    } else {
-      if (req.file == undefined) {
-        res.render("index", {
-          msg: "Error:No File Selected!"
-        });
-      } else {
-        res.render("index", {
-          msg: "File Uploaded!",
-          file: `uploads/${req.file.filename}`
-        });
-      }
-    }
-  });
-});
+//   if (mimetype && extname) {
+//     return cb(null, true);
+//   } else {
+//     cd("Error: Images only!");
+//   }
+// }
+
+// app.post("/upload", (res, req) => {
+//   upload(req, res, err => {
+//     if (err) {
+//       res.render("index", {
+//         msg: err
+//       });
+//     } else {
+//       if (req.file == undefined) {
+//         res.render("index", {
+//           msg: "Error:No File Selected!"
+//         });
+//       } else {
+//         res.render("index", {
+//           msg: "File Uploaded!",
+//           file: `uploads/${req.file.filename}`
+//         });
+//       }
+//     }
+//   });
+// });
 
 //<img src="">
 
