68725d5e2b08612d0ee60367aebae9f0c0f67480
Admin <masashilandjob@gmail.com>
2019-11-22 08:46:11 +1300

    heroku

--- server/client/src/components/api/apiReducer.js
+++ server/client/src/components/api/apiReducer.js
@@ -17,7 +17,7 @@ export default (state, action) => {
     case USER_LOADED:
       return {
         ...state,
-        isAuthenticated: false,
+        isAuthenticated:true,
         loading: false,
         users: action.payload
       };
