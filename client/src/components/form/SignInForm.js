8b3b396245fa032a7cba6b4ec15a9059ee9ae487
lianxu <lianxu@188.com>
2020-01-15 15:01:25 +1300

    modify the login page

--- client/src/components/form/SignInForm.js
+++ client/src/components/form/SignInForm.js
@@ -1,148 +1,77 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+
 import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import photo_loginpic from "../../img/loginpic.jpg";
+
 import { Redirect } from "react-router-dom";
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <p variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit">Canterbury Music Teacher</Link>{" "}
       {new Date().getFullYear()}
       {"."}
-    </Typography>
+    </p>
   );
 }
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: "100vh"
-  },
-  image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor: theme.palette.grey[50],
-    backgroundSize: "cover",
-    backgroundPosition: "center"
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
-}));
-
-export default function SignInForm({
+ const SignInForm = ({
   history,
   onSubmit,
   onChange,
   errors,
   user
-}) {
-  const classes = useStyles();
+}) => {
+ 
   if (history) {
-    return <Redirect to="/" />;
+    return <Redirect to="/" />
   }
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
 
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form onSubmit={onSubmit} className={classes.form}>
-            {errors.message && (
-              <p style={{ color: "red", fontSize: 13 }}>{errors.message}</p>
-            )}
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              value={user.email}
-              onChange={onChange}
-              autoComplete="email"
-              autoFocus
-            />
-            {errors.email && (
-              <p style={{ color: "red", fontSize: 13 }}>{errors.email}</p>
-            )}
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="password"
-              value={user.password}
-              onChange={onChange}
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-
-            {errors.password && (
-              <p style={{ color: "red", fontSize: 13 }}>{errors.password}</p>
-            )}
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item></Grid>
-            </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-          </form>
+    <div className="container">
+        <div className="row">
+            <div className="col s8 offset-s2 m6 offset-m3">
+                <div className="card center-align">
+                    <div className="card-image">
+                        <img className="activator" src={photo_loginpic} ></img>
+                    </div>
+                    <div className="card-tabs">
+                    <ul className="tabs tabs-fixed-width">
+                        <li className="tab"><a href="#test4">Admin</a></li>
+                    </ul>
+                    </div>
+                    <div className="card-content grey lighten-4">
+                    <div id="test4">
+                        <div className="row">
+                            <form action="" method="post">
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">account_circle</i>
+                                    <input id="email" name="email" type="text" className="validate"></input>
+                                    <label for="email">User Name</label>
+                                </div> 
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">vpn_key</i>
+                                    <input id="password" name="password" type="text" className="validate"></input>
+                                    <label for="password">Password</label>
+                                </div>  
+                                <div>
+                                    <button type="submit" className="waves-effect waves-light btn red"><i className="material-icons right">arrow_forward</i>Login</button>
+                                </div>                             
+                            </form>                          
+                        </div>                       
+                    </div>
+                    </div>
+                </div>            
+            </div>
         </div>
-      </Grid>
-    </Grid>
+    </div>
+  
+
+
+
   );
 }
-//    <pre>{JSON.stringify(user, null, 2)}</pre>
+
+export default SignInForm
