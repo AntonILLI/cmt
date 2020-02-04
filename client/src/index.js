4bc6ec97c5188dd7156395bd509c4b49d64861e5
Admin <you@example.com>
2020-02-05 12:34:04 +1300

    addd half past 12

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
@@ -10,9 +9,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-=======
-
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> d0b64319a84d7dce92c396baf9c2386b8f130adb
